0d0da4cc843610cf007be91f2cdb1d27c7fa464e
alincode <alin.code@gmail.com>
2021-10-19 22:52:10 +0800

    chore: add pino-pretty for improve log

--- start.js
+++ start.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 5000
 let app
 
 const start = async () => {
-  let appOpts = { logger: true }
+  let appOpts = {
+    logger: {
+      prettyPrint: true,
+    },
+  }
   // let appOpts = { logger: { level: 'info' } }
   app = build(appOpts)
   try {
